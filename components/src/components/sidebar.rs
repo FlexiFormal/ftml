d0d7718045993ad4ef4043cf7eb1506802e26636
Jazzpirate <raupi@jazzpirate.com>
2025-10-13 14:58:57 +0200

    fix

--- components/src/components/sidebar.rs
+++ components/src/components/sidebar.rs
@@ -224,10 +224,10 @@ fn content_drawer<B: SendBackend>() -> impl IntoView {
                     </DrawerHeaderTitleAction>
                     <Popover>
                         <PopoverTrigger slot>
-                            <Text>{let uri = uricl2; move || {
+                            <Text><span inner_html={let uri = uricl2; move || {
                                 let ttl = title.get();
                                 if ttl.is_empty() { uri.name.to_string()} else {ttl}
-                            }}</Text>
+                            }}/></Text>
                         </PopoverTrigger>
                         <Text>{uricl.to_string()}</Text>
                     </Popover>
